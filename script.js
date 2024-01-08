4582bf0e8c4bc46d2c0605319d57411ada9db7a0
Andrew Conklin <arconkli@gmail.com>
2024-01-07 23:43:14 -0600

    Update script.js

--- script.js
+++ script.js
@@ -9,13 +9,12 @@ const elts = {
 
 // The strings to morph between. You can change these to anything you want!
 const texts = [
-"Why",
-"is",
-"this",
-"so",
-"satisfying",
-"to",
-"watch?"];
+"Andrew",
+"Conklin",
+"Actor",
+"Writer",
+"Musician"
+];
 
 
 // Controls the speed of morphing.
